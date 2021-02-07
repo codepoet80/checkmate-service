ebe3307991756cc1dceb937b677eb8e498d75f19
codepoet80 <curator@webosarchive.com>
2021-02-06 20:23:14 -0500

    more icon attempts

--- tasks.php
+++ tasks.php
@@ -194,14 +194,16 @@ function check_response_for_errors($response)
 ?>
 <html>
 <head>
-<link rel="shortcut icon" sizes="192x192" href="nice-highres.png"> (recommended)
-<link rel="shortcut icon" sizes="128x128" href="niceicon.png">
-<link rel="apple-touch-icon" sizes="128x128" href="niceicon.png">
-<link rel="apple-touch-icon-precomposed" sizes="128x128" href="niceicon.png">
-
-<link rel="shortcut icon" sizes="256x256" href="images/icon-256.png"> (recommended)
+<link rel="shortcut icon" sizes="256x256" href="images/icon-256.png">
+<link rel="shortcut icon" sizes="196x196" href="images/icon-196.png">
+<link rel="shortcut icon" sizes="128x128" href="images/icon-128.png">
 <link rel="shortcut icon" href="favicon.ico">
-<link rel="icon" href="images/icon.png" type="image/png">
+<link rel="icon"type="image/png" href="images/icon.png" >
+<link rel="apple-touch-icon" href="images/icon.png"/>
+<link rel="apple-touch-startup-image" href="images/icon-256.png">
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
 <link rel="stylesheet" href="style.css">
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=1" />
 <meta http-equiv="pragma" content="no-cache">
