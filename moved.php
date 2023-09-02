1bea002e59b706e26cd080a9c7a722edcb12f573
codepoet80 <jon.wise@jonandnic.com>
2023-09-02 10:24:24 -0400

    notify of move

---
+++ moved.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>Check Mate Moved!</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Check Mate has moved!</h1>
+<p>Domain renewals were getting expensive, so this app has moved to a subdomain.</p>
+<p>If you're using the web app, uninstall and re-install at the new address: <a href="https://checkmate.wosa.link">checkmate.wosa.link</a></p>
+<p>If you're on webOS, get the new version of the app from the <a href="https://appcatalog.webosarchive.org">App Museum</a>!</p>
+</body>
+</html>
